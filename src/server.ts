fee3a30d6c92ae3cb6059b0af1d5a15b6a305f97
METHEW <yotoqxona.com@gmail.com>
2026-07-24 11:31:07 +0500

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,14 @@ console.log("<<<<  server is running  >>>>")
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT:", process.env.PORT)
-console.log("MONGO_URL:", process.env.MONGO_URL)
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3030;
+
+}).catch((err => console.log("Error on connection mongodb:", err)));

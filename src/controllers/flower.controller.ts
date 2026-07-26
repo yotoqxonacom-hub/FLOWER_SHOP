ddc8383407dce37ca8bf5c85df67e5bd1cd808b0
METHEW <yotoqxona.com@gmail.com>
2026-07-26 14:14:58 +0500

    feat: develop signup process

--- src/controllers/flower.controller.ts
+++ src/controllers/flower.controller.ts
@@ -2,8 +2,11 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
+import { MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
 
 const flowerController: T = {};
+const memberService = new MemberService();
 
 flowerController.goHome = (req: Request, res: Response) => {
     try {
@@ -41,12 +44,18 @@ flowerController.processLogin = (req: Request, res: Response) => {
     }
 };
 
-flowerController.processSignup = (req: Request, res: Response) => {
+flowerController.processSignup = async (req: Request, res: Response) => {
     try {
         console.log("processSignup:");
-        res.send("Done")
+        const newMember: MemberInput = req.body;
+
+        newMember.memberType = MemberType.FLOWER;
+        const result = await memberService.processSignup(newMember);
+
+        res.send(result)
     } catch (err) {
         console.log(" Error, processSignup:", err)
+        res.send(err)
     }
 };
 
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,31 @@
-class MemberService {
-    constructor() {
+import { MemberType } from "../libs/enums/member.enum";
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { Member, MemberInput } from "../libs/types/member";
+import MemberSchemaModel from "../schema/MemberSchemaModel";
 
+class MemberService {
+    private readonly memberModel;
+    constructor() {
+        this.memberModel = MemberSchemaModel;
     }
+
+    public async processSignup(input: MemberInput): Promise<Member> {
+        const exist = await this.memberModel
+            .findOne({ memberType: MemberType.FLOWER })
+            .exec();
+
+        if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        try {
+            const result = await this.memberModel.create(input);
+
+            result.memberPassword = "";
+
+            return result
+        } catch (err) {
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        }
+    }
+
 }
 
 

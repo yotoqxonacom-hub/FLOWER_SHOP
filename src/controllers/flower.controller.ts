21565b6fc50e1008b73ba7508c752d302df9dd33
METHEW <yotoqxona.com@gmail.com>
2026-07-27 12:21:28 +0500

    feat: frontend related css, js and img created

--- src/controllers/flower.controller.ts
+++ src/controllers/flower.controller.ts
@@ -11,7 +11,7 @@ const memberService = new MemberService();
 flowerController.goHome = (req: Request, res: Response) => {
     try {
         console.log("goHome:");
-        res.send("Home Page")
+        res.render("home");
     } catch (err) {
         console.log(" Error, goHome:", err)
     }
@@ -20,7 +20,7 @@ flowerController.goHome = (req: Request, res: Response) => {
 flowerController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup:");
-        res.send("Signup Page")
+        res.render("signup");
     } catch (err) {
         console.log(" Error, getSignup:", err)
     }
@@ -30,7 +30,7 @@ flowerController.getSignup = (req: Request, res: Response) => {
 flowerController.getLogin = (req: Request, res: Response) => {
     try {
         console.log("getLogin:");
-        res.send("Login Page")
+        res.render("login");
     } catch (err) {
         console.log(" Error, getLogin:", err)
     }
--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,8 @@ mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
     const PORT = process.env.PORT ?? 3030;
     app.listen(PORT, function () {
-        console.log(`The server is running succesfully on port ${PORT}`);
+        console.info(`The server is running succesfully on port ${PORT}`);
+        console.info(` admin project on http://localhost:${PORT}/admin \n`)
     })
 
 }).catch((err => console.log("Error on connection mongodb:", err)));

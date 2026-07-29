4b076a508cc874cd735ce3561019a4d15d6e0608
METHEW <yotoqxona.com@gmail.com>
2026-07-29 07:39:17 +0500

    fix: create sessions on login and signup proccess

--- src/controllers/flower.controller.ts
+++ src/controllers/flower.controller.ts
@@ -2,7 +2,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const flowerController: T = {};
@@ -37,30 +37,37 @@ flowerController.getLogin = (req: Request, res: Response) => {
 };
 
 
-flowerController.processSignup = async (req: Request, res: Response) => {
+flowerController.processSignup = async (req: AdminRequest, res: Response) => {
     try {
         console.log("processSignup:");
         const newMember: MemberInput = req.body;
         newMember.memberType = MemberType.FLOWER;
         const result = await memberService.processSignup(newMember);
         // TODO SESSION AUTHENTICATION
+        req.session.member = result;
+        req.session.save(() => {
+            res.send(result);
+        });
 
-        res.send(result)
+
     } catch (err) {
         console.log(" Error, processSignup:", err)
         res.send(err)
     }
 };
 
-flowerController.processLogin = async (req: Request, res: Response) => {
+flowerController.processLogin = async (req: AdminRequest, res: Response) => {
     try {
         console.log("processLogin:");
         const input: LoginInput = req.body,
             result = await memberService.processLogin(input);
         // TODO SESSION AUTHENTICATION
+        req.session.member = result;
+        req.session.save(() => {
+            res.send(result);
+        });
 
 
-        res.send(result)
     } catch (err) {
         res.send(err)
         console.log(" Error, processLogin:", err)

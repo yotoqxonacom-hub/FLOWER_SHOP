08f76ad4af3506a197de24cdf879b77a8ba70baa
METHEW <yotoqxona.com@gmail.com>
2026-08-08 19:52:41 +0900

    fix: modify signup page

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,31 @@
 console.log("Signup frontend javascript file");
-$(function () { });
+
+$(function () {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function () {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            console.log("uploadFile:", uploadFile);
+            const fileType = uploadFile["type"];
+            const validImageTypes = ["image/jpeg", "image/png", "image/jpg"];
+            if (!validImageTypes.includes(fileType)) {
+                alert("Please select a valid image file (JPEG, PNG, JPG)!");
+            } else {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                        .attr("src", URL.createObjectURL(uploadFile))
+                        .addClass("success");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+});
+
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -19,5 +45,14 @@ function validateSignupForm() {
         alert("Passwords do not match, please check and try again!");
         return false;
     }
+
+    const memberImage = $(".member-image").get(0).files[0].name
+        ? $(".member-image").get(0).files[0].name
+        : null;
+    if (!memberImage) {
+        alert("Please insert restaurant image!");
+        return false;
+    }
+
 
 }

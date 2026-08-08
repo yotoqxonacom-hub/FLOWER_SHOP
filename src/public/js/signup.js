b2b1bc33f57e7c3663861adde5cf3696b4ae34cb
METHEW <yotoqxona.com@gmail.com>
2026-08-08 19:19:37 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,23 @@
 console.log("Signup frontend javascript file");
+$(function () { });
+
+function validateSignupForm() {
+    const memberNick = $(".member-nick").val();
+    const memberPhone = $(".member-phone").val();
+    const memberPassword = $(".member-password").val();
+    const confirmPassword = $(".confirm-password").val();
+
+    if (memberNick === "" ||
+        memberPhone === "" ||
+        memberPassword === "" ||
+        confirmPassword === "") {
+        alert("Please fill in all required fields!");
+        return false;
+    }
+
+    if (memberPassword !== confirmPassword) {
+        alert("Passwords do not match, please check and try again!");
+        return false;
+    }
+
+}

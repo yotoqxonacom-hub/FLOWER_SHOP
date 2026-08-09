2a6fd1f7a30ddc17042e7bb3efeb543d9b207878
METHEW <yotoqxona.com@gmail.com>
2026-08-09 13:20:00 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -11,6 +11,27 @@ $(function () {
         $("#process-btn").css("display", "flex");
     });
 
+    $(".new-product-status").on("change", async function (e) {
+        const id = e.target.id;
+        const productStatus = $(`#${id}.new-product-status`).val();
+        console.log("id:", id);
+        console.log("productStatus:", productStatus);
+
+        try {
+            const response = await axios.post(`/admin/product/${id}`, { productStatus: productStatus });
+            console.log("response:", response);
+            const result = response.data;
+            if (result.data) {
+                console.log("Product updated!");
+                $(`#${id}.new-product-status`).blur();
+            } else alert("Product update failed!");
+        } catch (err) {
+            console.log(err);
+            alert("Product update failed!");
+        }
+    });
+
+
 });
 
 
